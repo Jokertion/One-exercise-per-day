8c0409b4b659780836ef2a75f9a5ffcf5107b8c4
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-09-29 23:19:21 +0800

    月份抽象成列表--判断某天是这年第几天

--- day3.py
+++ day3.py
@@ -2,20 +2,27 @@
 # 题目：输入某年某月某日，判断这一天是这一年的第几天？
 # Input 2018 9 18
 # Output 220
+
 month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
 def date_account(date):
     y, m, d = date.split()
     y, m, d = int(y), int(m), int(d)
     print("y =", y, "m =", m, "d =", d)
+    
     # 判断闰年否
     if y % 4 == 0:
         month[1] = 29
+        
     # 计算本月之前的总天数
     num = 0
     for i in month[0:m-1]:
         # print('遍历到的i', i)
         num += i
+        
     # 加上本月的天数 计算总天数
     all_num = d + num
     print('it is the %dth day' % all_num)
+    
+    
 date_account('2020 1 19')
