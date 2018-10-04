f4d26f39403ba7e3a382030acf4dabfd012616ba
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-10-04 22:46:15 +0800

    给定一个字典，判断该字典是否在list中存在

--- day7.py
+++ day7.py
@@ -1,7 +1,12 @@
-#  给定一个字典，对字典中的key，value互换。
-# 要求： 一行代码实现（字典推导式实现）
-# output: {"v1": "k1", "v2": "k2", "v3": "k3", "v4": "k4"}
+#  给定一个字典，判断该字典是否在list中存在。
+#  output: True or False
 
-_dict = {"k1": "v1", "k2": "v2", "k3": "v3", "k4": "v4"}
+_dict = {"x": "x"}
+_list = [{"x": "x"}, {"y": "y"}, {"z": "z"}]
 
-_dict = {v: k for k, v in _dict.items()}
+for dic in _list:
+    if dic == _dict:
+        print("True")
+        break
+else:
+    print("False")
